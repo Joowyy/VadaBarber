c09a60fe7bffcd0567a1b104ad974f4d64957f80
Joowyy <joelsanchezfernandez05@gmail.com>
2025-11-04 22:14:09 +0100

    Casi finish el layout login

--- app/src/main/java/com/example/vadabarber/LoginActivity.kt
+++ app/src/main/java/com/example/vadabarber/LoginActivity.kt
@@ -2,12 +2,18 @@ package com.example.vadabarber
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.example.vadabarber.databinding.LoginActivityBinding
 
 class LoginActivity : ComponentActivity() {
+
+    private lateinit var binding : LoginActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+
+        binding = LoginActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 
